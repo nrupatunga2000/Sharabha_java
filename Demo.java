b3944f491297974ac4509846a9fe94341cee2b7d
Nrupatunga <57655312+nrupatunga2000@users.noreply.github.com>
2024-10-27 09:40:00 +0530

    Add files via upload
    added all my files

---
+++ Demo.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Demo {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the name ");
+    String name = sc.nextLine();
+    System.out.println("enter a number");
+    int age = sc.nextInt();
+    System.out.println(" the name: "+ name + " age " + age);
+}
+    
+}
---
+++ pattern.java
@@ -0,0 +1,15 @@
+public class pattern {
+    public static void main(String[] args) {
+        int n = 4;
+        int m = 5;
+        
+        //outer loop
+        for(int i=1; i<n; i++){
+            for(int j=0;j<m;j++){
+                System.out.print("*");
+            }
+            System.out.println("*");
+        }
+
+    }
+}
