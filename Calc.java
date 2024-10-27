85532f96c18ab54ecbc6c804720ae56b75c27799
Nrupatunga <nrupatunga619@gmail.com>
2024-10-27 12:17:43 +0530

    created an array

---
+++ Calc.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+/**
+ * clacual
+ */
+public class Calc {
+    int 
+
+    
+}
+
+public class array {
+public static void main(String[] args) {
+    array obj = new array();
+    System.out.println(obj.name);
+}
+    
+}
