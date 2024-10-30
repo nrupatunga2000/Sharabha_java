2cf02f72675331b7faa692c75d12712a62e74bcc
Nrupatunga <nrupatunga619@gmail.com>
2024-10-30 15:55:17 +0530

    tried to impliment Switch case

---
+++ Strings.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Strings {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("enetr a number");
+        int week = scn.nextInt();
+        switch (week) {
+            case 1:
+            System.out.println("Hi Tanu");
+                
+                break;
+            
+            case 2:
+            System.out.println("Hi Nrupatunga");
+                
+                break;
+        
+            default:
+                break;
+        }
+    }
+}
