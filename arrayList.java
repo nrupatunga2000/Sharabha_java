4b8afc2829405ff163b5a1a2843f6ba7124df2fe
Nrupatunga <nrupatunga619@gmail.com>
2024-10-31 21:13:45 +0530

    arays of objects is created. a collection implimentation

---
+++ arrayList.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class students{
+    String name;
+    int rollNum;
+    int marks;
+
+}
+public class arrayList {
+    public static void main(String[] args) {
+        students s1 = new students();
+        s1.name = "Nrupatunga";
+        s1.marks = 50;
+        s1.rollNum = 21;
+
+        students s2 = new students();
+        s2.marks = 24;
+        s2.name = "samy";
+        s2.rollNum = 22;
+
+        students s3 = new students();
+        s2.marks = 36;
+        s2.name = "Tanu";
+        s3.rollNum = 20;
+
+        students stud[] = new students[3];
+        stud[0] = s1;
+        stud[1] = s2;
+        stud[2] = s3
+    }
+}
